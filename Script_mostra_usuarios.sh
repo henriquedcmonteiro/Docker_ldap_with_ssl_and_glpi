6e908c2a05785b32930d335c45cf35a52ee5928d
Luiz Henrique de Campos Monteiro <139652963+henriquedcmonteiro@users.noreply.github.com>
2024-12-13 21:24:19 -0300

    Create Script_mostra_usuarios.sh

---
+++ Script_mostra_usuarios.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Configurações LDAP
+LDAP_SERVER="ldaps://ldap-server"
+LDAP_ADMIN_DN="cn=admin,dc=empresa,dc=local"
+BASE_DN="ou=users,dc=empresa,dc=local" # Ajuste conforme necessário
+
+# Solicitar a senha do administrador LDAP
+read -s -p "Digite a senha do administrador LDAP: " LDAP_ADMIN_PASS
+echo
+
+# Validar a senha fornecida
+ldapsearch -x -H "LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    error_exit "Senha do administrador LDAP incorreta."
+fi
+
+# Busca todos os usuários e seus UIDs
+ldapsearch -x -H "LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" "(objectClass=posixAccount)" uid uidNumber | grep -E "uid:|uidNumber:" | paste - - | awk '{print $4 "\t" $2}'
