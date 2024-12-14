5668a9f226b6b645d2fc20539b6d203d38d6c68e
Luiz Henrique de Campos Monteiro <139652963+henriquedcmonteiro@users.noreply.github.com>
2024-12-13 21:11:09 -0300

    Create Script_mostrar_grupos.sh

---
+++ Script_mostrar_grupos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Configurações LDAP
+LDAP_SERVER="ldaps://ldap-server"
+LDAP_ADMIN_DN="cn=admin,dc=djsystem,dc=local"
+BASE_DN="ou=groups,dc=djsystem,dc=local"
+
+# Função para exibir mensagens de erro
+function error_exit() {
+    echo "Erro: $1" >&2
+    exit 1
+}
+
+# Solicitar a senha do administrador LDAP
+read -s -p "Digite a senha do administrador LDAP: " LDAP_ADMIN_PASS
+echo
+
+# Validar a senha fornecida
+ldapsearch -x -H "$LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    error_exit "Senha do administrador LDAP incorreta."
+fi
+
+# Busca todos os grupos e seus GIDs
+ldapsearch -x -H "$LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" "(objectClass=posixGroup)" gidNumber cn | grep -E "cn:|gidNumber:" | paste - - | awk '{print $4 "\t" $2}'
+
