05a0c95b1c7685ee4bd32500bc615a32e7d02f4b
Luiz Henrique de Campos Monteiro <139652963+henriquedcmonteiro@users.noreply.github.com>
2024-12-30 11:35:08 -0300

    Update Script_mostra_usuarios.sh

--- Script_mostra_usuarios.sh
+++ Script_mostra_usuarios.sh
@@ -3,17 +3,26 @@
 # Configurações LDAP
 LDAP_SERVER="ldaps://ldap-server"
 LDAP_ADMIN_DN="cn=admin,dc=empresa,dc=local"
-BASE_DN="ou=users,dc=empresa,dc=local" # Ajuste conforme necessário
+BASE_DN="ou=groups,dc=empresa,dc=local"  
+
+# Função para exibir mensagens de erro
+function error_exit() {
+    echo "Erro: $1" >&2
+    exit 1
+}
 
 # Solicitar a senha do administrador LDAP
 read -s -p "Digite a senha do administrador LDAP: " LDAP_ADMIN_PASS
 echo
 
 # Validar a senha fornecida
-ldapsearch -x -H "LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" > /dev/null 2>&1
+ldapsearch -x -H "$LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
     error_exit "Senha do administrador LDAP incorreta."
 fi
 
-# Busca todos os usuários e seus UIDs
-ldapsearch -x -H "LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" -b "$BASE_DN" "(objectClass=posixAccount)" uid uidNumber | grep -E "uid:|uidNumber:" | paste - - | awk '{print $4 "\t" $2}'
+# Busca todos os grupos e seus GIDs
+ldapsearch -x -H "$LDAP_SERVER" -D "$LDAP_ADMIN_DN" -w "$LDAP_ADMIN_PASS" \
+    -b "$BASE_DN" "(objectClass=posixGroup)" gidNumber cn | grep -E "cn:|gidNumber:" | \
+    paste - - | awk '{print $4 "\t" $2}'
+
